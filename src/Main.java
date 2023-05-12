a30547729aeb3346dd702a9c24d36d74ea7d92ec
Olexander Zhurba <starkyzh@gmail.com>
2023-05-12 17:48:16 +0200

    some format

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,35 @@
 public class Main {
 
-  /*
-  Напишите интерфейсы "Умеет летать" CanFly с методом fly(), "Умеет бегать" CanRun с методом
-  run и "умеет плавать" CanSwim с методом swim и реализуйте их в классе "Супермен" Superman.
-   */
-  public static void main(String[] args) {
-    Superman superman = new Superman("Superman from Marvell!");
-    System.out.println(superman + " and " + superman.fly());
-    System.out.println(superman + " and " + superman.run());
-    System.out.println(superman + " and " + superman.swim());
-    System.out.println("============");
-    Human forest = new Human("soldier Forrest Gump!");
-    System.out.print(forest + forest.run() + "\n");
-    System.out.print(forest + forest.swim() + "\n");
-    System.out.println("============");
-    Duck duck = new Duck("Duck, Scrooge Duck!");
-    System.out.println(duck + " and " + duck.fly());
-    System.out.println(duck + " and " + duck.run());
-    System.out.println(duck + " and " + duck.swim());
-    System.out.println("============");
-    Penguin penguin = new Penguin("Kovalski, sir!");
-    System.out.println(penguin + " and " + penguin.run());
-    System.out.println(penguin + " and " + penguin.swim());
-    System.out.println("============");
+    /*
+    Напишите интерфейсы "Умеет летать" CanFly с методом fly(), "Умеет бегать" CanRun с методом
+    run и "умеет плавать" CanSwim с методом swim и реализуйте их в классе "Супермен" Superman.
+     */
+    public static void main(String[] args) {
+        Superman superman = new Superman("Superman from Marvell!");
+        System.out.println(superman + " and " + superman.fly());
+        System.out.println(superman + " and " + superman.run());
+        System.out.println(superman + " and " + superman.swim());
+        System.out.println("============");
 
-    Airplane airplane = new Airplane("F-14 «Tomcat»!");
-    System.out.println(airplane + " and " + airplane.fly());
-    System.out.println(airplane + " and " + airplane.swim());
-    System.out.println("============");
+        Human forest = new Human("soldier Forrest Gump!");
+        System.out.print(forest + forest.run() + "\n");
+        System.out.print(forest + forest.swim() + "\n");
+        System.out.println("============");
 
+        Duck duck = new Duck("Duck, Scrooge Duck!");
+        System.out.println(duck + " and " + duck.fly());
+        System.out.println(duck + " and " + duck.run());
+        System.out.println(duck + " and " + duck.swim());
+        System.out.println("============");
 
-  }
+        Penguin penguin = new Penguin("Kovalski, sir!");
+        System.out.println(penguin + " and " + penguin.run());
+        System.out.println(penguin + " and " + penguin.swim());
+        System.out.println("============");
+
+        Airplane airplane = new Airplane("F-14 «Tomcat»!");
+        System.out.println(airplane + " and " + airplane.fly());
+        System.out.println(airplane + " and " + airplane.swim());
+        System.out.println("============");
+    }
 }
